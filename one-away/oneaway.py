215cce0556bad1f04c8bf100ee9f55ff03687187
Jade Hayes <Jade.e.Hayes@gmail.com>
2018-04-16 22:24:40 +0000

    addedoneaway

---
+++ one-away/oneaway.py
@@ -0,0 +1,82 @@
+"""Given two strings, are they, at most, one edit away?
+
+    >>> one_away("make", "make")
+    True
+
+    >>> one_away("make", "fake")
+    True
+
+    >>> one_away("task", "take")
+    False
+
+    >>> one_away("ask" ,"asks")
+    True
+
+    >>> one_away("asks", "ask")
+    True
+
+    >>> one_away("act", "tact")
+    True
+
+    >>> one_away("fat", "fact")
+    True
+
+    >>> one_away("yes", "no")
+    False
+
+"""
+
+
+def one_away(w1, w2):
+    """Given two strings, are they, at most, one edit away?"""
+    # set up a dictionary count for 1 & 2
+    # fail fast by checking the length, if one word is > the other by more than
+    # 1 char, we fail fast. 
+    # set up a while loop to compare the indicies, add one to the index if matches
+    wrong = 0
+
+    # Check to see if the length of the words vary by more than one
+    if abs(len(w1) - len(w2)) > 1:
+        return False
+
+    # check to see which word is shorter, in our case, the shorter word will always
+    # be w2
+
+    if len(w1) < len(w2):
+        w1, w2 = w2, w1
+
+    i = 0
+    j = 0
+
+    # while the second index is shorter than the shorter word
+    while j < len(w2):
+        if w1[i] != w2[j]:
+            # we found a wrong letter
+            wrong += 1
+
+            if wrong > 1:
+                return False
+
+            # lets move back to the next iteration of the longer string
+            i += 1
+
+            # if they are the same length,increase j too
+            if len(w1) == len(w2):
+                j += 1
+
+        else:
+            i += 1
+            j += 1
+    return True
+
+
+
+
+
+    
+
+
+if __name__ == '__main__':
+    import doctest
+    if doctest.testmod().failed == 0:
+        print "\n*** ALL TESTS PASSED; NICE JOB! ***\n"
