8719b334509e33f84face72f519e2385c1a0fff8
Jade Hayes <jade.e.hayes@gmail.com>
2018-04-27 17:13:30 +0000

    javascript party

--- is_happy.py
+++ is_happy.py
@@ -1,9 +1,14 @@
 def is_happy(n):
+    # create a set seen, for the sum of the squares we have
     seen = set()
+    # while the number we get isn't 1
     while n!=1:
+        # n is the sum of the digits squared and added together
         n = sum(int(d)**2 for d in str(n))
+        # if n isn't in the seen set, add it.  If it is return False b/c it'll have us in a loop
         if n not in seen:
             seen.add(n)
         else:
             return False
+    # if this all passes return true
     return True
